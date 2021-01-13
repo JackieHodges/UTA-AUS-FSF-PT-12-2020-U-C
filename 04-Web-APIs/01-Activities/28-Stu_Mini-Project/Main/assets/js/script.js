8fea7c7c9884b7f8f9f20af658a638951bc94ede
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-13 08:38:47 -0600

    my changes

--- 04-Web-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
+++ 04-Web-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
@@ -1,1 +1,42 @@
 //javascript code goes here
+
+var button = document.querySelector(".start-button");
+var timer = document.querySelector(".timer-count");
+var winLose = document.querySelector(".word-blanks");
+var secondsLeft = 10;
+var loseCount = document.querySelector(".lose");
+
+
+button.addEventListener("click", function (event){
+    event.preventDefault();
+    startTimer();
+    guessLetters();
+
+})
+
+function startTimer(){
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft ;
+    
+        if(secondsLeft === 0) {
+          // Stops execution of action at set interval
+          clearInterval(timerInterval);
+          // Calls function to create and append image
+          winLose.innerHTML = "you lose";
+        }
+    
+      }, 1000);
+};
+
+function guessLetters(event){
+  // The key property holds the value of the key press
+  var keyPress = event.key;
+  // The code property holds the key's code 
+  var keyCode = event.code;
+  // Updates content on page
+  document.querySelector(".word-guess").textContent = keyPress;
+  document.querySelector("#code").textContent = keyCode;
+  document.querySelector("#status").textContent = "KEYDOWN Event";
+}
+
