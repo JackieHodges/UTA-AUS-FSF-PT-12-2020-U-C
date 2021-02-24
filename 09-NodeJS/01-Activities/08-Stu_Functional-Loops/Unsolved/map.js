fef0dca13047d99e3ad712609cbe20aeb13c12dc
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-02-24 15:46:42 -0600

    my code

--- 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
+++ 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
@@ -12,6 +12,7 @@ console.log(originalArray);
 
 // TODO: Describe how map is working in the example below. What will the value of tripledArray be?
 const tripledArray = originalArray.map(data => data * 3);
+console.log(tripledArray);
 
 // TODO: Describe how map is working in the example below. What will the value of oddOrEven be?
 const oddOrEven = originalArray.map(num => {
--- 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,13 +1,17 @@
 // music should be an object with title, artist, and album properties
 const music = {
-  // code here
+  title: "Save Your Tears",
+  artist: "The Weeknd",
+  albumn: "After Hours"
 };
 
 // Write code between the <div> tags to output the data from the music object above.
 // Use an h2 element for the title and a p element for artist and title
 const songSnippet = `
   <div class="song">
-
+  <h2>${music.title}</h2>
+  <p>${music.artist}</p>
+  <p>${music.albumn}</p>
   </div>
 `;
 
